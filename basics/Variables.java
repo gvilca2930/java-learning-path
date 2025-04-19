d5e700d1b134f088f8df7b742898461b1a4aa9b6
George Gregory Vilca Guerra <george.vilca29@gmail.com>
2025-04-18 20:09:39 -0500

    Se crea la clase Variables comprendiendo los tipos de datos

---
+++ basics/Variables.java
@@ -0,0 +1,18 @@
+package basics;
+
+public class Variables {
+
+    public static void main(String[] args) {
+        int edad = 28;
+        double saldo = 10.49;
+        boolean trabajo = true;
+        char inicialNombre = 'G';
+        String nombre = "George";
+    
+        System.out.println("El nombre del trabajador es: "+nombre);
+        System.out.println("La edad del trabajador es: "+edad);
+        System.out.println("La inicial del trabajador es: "+ inicialNombre);
+        System.out.println("Trabajo actual: "+trabajo);
+        System.out.println("El trabajador tiene un saldo de: "+saldo);
+    }
+}
