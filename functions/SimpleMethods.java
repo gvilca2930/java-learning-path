18b581fa4109a18ad3d6ed5e3df4d6ecd6b930f8
George Gregory Vilca Guerra <george.vilca29@gmail.com>
2025-04-18 22:32:16 -0500

    Comprende metodos y estructura

---
+++ functions/SimpleMethods.java
@@ -0,0 +1,20 @@
+package functions;
+
+public class SimpleMethods {
+
+    public static void main(String[] args) {
+        mostrarTitulo();
+        mostrarMenu();
+    }
+
+    public static void mostrarTitulo(){
+        System.out.println("====== CALCULADORA BASICA ======");
+    }
+
+    public static void mostrarMenu(){
+        System.out.println("1. Sumar");
+        System.out.println("2. Restar");
+        System.out.println("3. Salir"); 
+    }
+    
+}
